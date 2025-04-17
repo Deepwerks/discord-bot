46bef497d454bd6cefd8f5e945da44d0fe58be87
Figura Árpád <figuraarpad03@gmail.com>
2025-04-17 20:15:01 +0200

    misc

--- src/services/utils/getGuildLang.ts
+++ src/services/utils/getGuildLang.ts
@@ -1,6 +1,5 @@
 import GuildConfig from "../../base/schemas/GuildConfig";
 import { getCachedLang, setCachedLang } from "../cache/langCache";
-import logger from "../logger";
 
 export async function getGuildLang(guildId?: string): Promise<string> {
   if (!guildId) return "en";
