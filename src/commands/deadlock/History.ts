d40e2bd96d452fba7407a83f4332f1df0a091d8e
Figura Árpád <figuraarpad03@gmail.com>
2025-04-23 20:01:59 +0200

    set client activity

--- src/commands/deadlock/History.ts
+++ src/commands/deadlock/History.ts
@@ -25,7 +25,7 @@ export default class History extends Command {
         PermissionsBitField.Flags.UseApplicationCommands,
       dm_permission: true,
       cooldown: 3,
-      dev: true,
+      dev: false,
       options: [
         {
           name: "player",
