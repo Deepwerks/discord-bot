48991ce11e7dab619357579ab1070c2927a0727d
Figura Árpád <figuraarpad03@gmail.com>
2025-06-21 11:44:42 +0200

    feat: randomize players

--- src/commands/deadlock/CreateLobby.ts
+++ src/commands/deadlock/CreateLobby.ts
@@ -29,6 +29,13 @@ export default class CreateLobby extends Command {
   async Execute(interaction: ChatInputCommandInteraction, t: TFunction<'translation', undefined>) {
     const modal = new ModalBuilder().setCustomId('createLobby').setTitle('Lobby Settings');
 
+    const lobbyNameInput = new TextInputBuilder()
+      .setCustomId('lobby_name_input')
+      .setLabel('Lobby name')
+      .setPlaceholder(`${interaction.user.displayName}'s lobby`)
+      .setStyle(TextInputStyle.Short)
+      .setRequired(true);
+
     const maxPlayersInput = new TextInputBuilder()
       .setCustomId('max_players_input')
       .setLabel(t('commands.create_lobby.max_players'))
@@ -37,7 +44,10 @@ export default class CreateLobby extends Command {
       .setValue('12')
       .setRequired(true);
 
-    modal.addComponents(new ActionRowBuilder<TextInputBuilder>().addComponents(maxPlayersInput));
+    modal.addComponents(
+      new ActionRowBuilder<TextInputBuilder>().addComponents(lobbyNameInput),
+      new ActionRowBuilder<TextInputBuilder>().addComponents(maxPlayersInput)
+    );
 
     await interaction.showModal(modal);
   }
